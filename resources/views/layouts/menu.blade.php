34aabdb33c7d7c616d4a419dcf08963e5185b33e
trabajotterminal <jairsaidds@gmail.com>
2018-04-04 11:57:16 -0500

    Fix an important bug - related to the responsive menu.

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,3 +1,4 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 @php
     $menu_classes       = [];
     $menu_classes[0]    = $page == 'index' ? 'active' : '';
