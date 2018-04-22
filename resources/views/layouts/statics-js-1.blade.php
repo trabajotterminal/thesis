22c0d34c407681877d63813d9cda51fac5d01fec
trabajotterminal <trabajotterminal@gmail.com>
2018-04-21 19:15:40 -0500

    Fix a bug related to the responsive menu.

--- resources/views/layouts/statics-js-1.blade.php
+++ resources/views/layouts/statics-js-1.blade.php
@@ -3,4 +3,4 @@
 <script src="{{ URL::asset('/js/jquery-ui.js')}}"></script>
 <script src="{{ URL::asset('/js/scrolltotop/totop.js')}}"></script>
 <script src="{{ URL::asset('/js/index.js')}}"></script>
-
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script>
--- resources/views/register_questionnaire_manually.blade.php
+++ resources/views/register_questionnaire_manually.blade.php
@@ -99,7 +99,7 @@
 @endsection
 
 @section('statics-js')
-    @include('layouts/statics-js-1')
+    @include('layouts/statics-js-2')
     <script src="/ace-builds/src-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
     <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
     <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script>
