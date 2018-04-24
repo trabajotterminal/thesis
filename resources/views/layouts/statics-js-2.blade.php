d7a6bfa330589d6e16871604c5b11e92f69527d1
trabajotterminal <jairsaidds@gmail.com>
2018-04-24 13:11:23 -0500

    Fix an important bug related to the menu design.

--- resources/views/layouts/statics-js-2.blade.php
+++ resources/views/layouts/statics-js-2.blade.php
@@ -1,5 +1,8 @@
+<script src="{{ URL::asset('/js/bootstrap/bootstrap.min.js')}}"></script>
 <script src="{{ URL::asset('/js/jquery.min.js')}}"></script>
 <script src="{{ URL::asset('/js/jquery-ui.js')}}"></script>
 <script src="{{ asset('/js/scrolltotop/totop.js')}}"></script>
 <script src="{{ asset('/js/category.js')}}"></script>
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script>
+
 
