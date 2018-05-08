017377b74aa3ac55cf40852fcd0ace6bf8b5f11d
trabajotterminal <trabajotterminal@gmail.com>
2018-05-07 19:15:26 -0500

    fix bug.

--- resources/views/layouts/statics-js-tags.blade.php
+++ resources/views/layouts/statics-js-tags.blade.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="{{ URL::asset('/css/tagit.ui-zendesk.css')}}"  type="text/css" />
 <script src="{{ URL::asset('/js/jquery_1.10.2.min.js')}}" type="text/javascript" charset="utf-8"></script>
-<script src="{{ URL::asset('/js/jquery-ui_1.9.2.min')}}" type="text/javascript" charset="utf-8"></script>
+<script src="{{ URL::asset('/js/jquery-ui_1.9.2.min.js')}}" type="text/javascript" charset="utf-8"></script>
 <script src="{{ URL::asset('/js/tag-it.js')}}"></script>
 <link rel="stylesheet" href="{{ URL::asset('/css/jquery.tagit.css')}}"  type="text/css" />
